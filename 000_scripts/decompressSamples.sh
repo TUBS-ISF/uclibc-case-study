9df86da2478cb6604a59bc8ba39fc9e81c3c6a05
Tobias Pett <pett@gpserv.isf.cs.tu-bs.de>
2021-02-22 18:29:02 +0100

    update scripts

--- 000_scripts/decompressSamples.sh
+++ 000_scripts/decompressSamples.sh
@@ -9,7 +9,7 @@ PATH_DATA=${PATH_CURRENT}/020_samples
 #iterate over all timestamps and decompress samples
 for year in ${PATH_DATA}/*; do
 	cd ${year}
-for timestamp in ${PATH_DATA}/*.tar.gz; do
+for timestamp in ${year}/*.tar.gz; do
 	tar -xzvf ${timestamp}
 done
 done
